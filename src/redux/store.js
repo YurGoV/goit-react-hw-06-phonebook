f9d817b0afdda2a12daa2611999ec324716161e8
YurGoV <yurgov@gmail.com>
2023-01-08 18:30:39 +0200

    refactorToCreateSlice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import {configureStore} from '@reduxjs/toolkit';
-import {createAction, createReducer} from '@reduxjs/toolkit';
+import {createAction, createReducer, createSlice} from '@reduxjs/toolkit';
 
 const initialContacts = [{
   id: '1',
@@ -12,15 +12,27 @@ const initialContacts = [{
     number: '000-000-0001',
   },];
 
-export const addContact = createAction('contact/add');
-export const delContact = createAction('contact/del');
+const contactsSlice = createSlice({name: 'contacts',
+  initialState: initialContacts,
+  reducers: {
+  addContact(state, action) {return [...state, action.payload]},
+    delContact(state, action) {
+      return state.filter(item => item.id !== action.payload)
+    },
+}
+})
+
+export const {addContact, delContact} = contactsSlice.actions
+
+// export const addContact = createAction('contact/add');
+// export const delContact = createAction('contact/del');
 export const setFilter = createAction('filter/set');
 
-const contactsReducer = createReducer(initialContacts, {
-  [addContact]: (state, action) => [...state, action.payload],
-  [delContact]: (state, action) => state.filter(item => item.id !== action.payload),
-
-})
+// const contactsReducer = createReducer(initialContacts, {
+//   [addContact]: (state, action) => [...state, action.payload],
+//   [delContact]: (state, action) => state.filter(item => item.id !== action.payload),
+//
+// })
 
 const filterReducer = createReducer('', {
   setFilter: (state, action) => state = action.payload,
@@ -29,7 +41,8 @@ const filterReducer = createReducer('', {
 
 export const store = configureStore({
   reducer: {
-    contacts: contactsReducer,
+    // contacts: contactsReducer,
+    contacts: contactsSlice.reducer,
     filter: filterReducer,
   },
 })
